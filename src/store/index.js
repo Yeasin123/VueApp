455b889df3a1c41df8511d197a24a34f826a2de1
Yeasin123 <yeasin15-1037@diu.edu.bd>
2022-01-16 13:46:04 +0600

    layout setup

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,40 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+    state: {
+        tasks: [{
+                id: 1,
+                title: 'Wake Up',
+                done: false,
+            },
+            {
+                id: 2,
+                title: 'Get Apple',
+                done: false,
+            },
+            {
+                id: 3,
+                title: 'Eat Apple',
+                done: false,
+            }
+        ],
+    },
+    mutations: {
+        ADD_TASK(state, task) {
+            state.tasks.push(task)
+        },
+        DELETE_TASK(state, id) {
+            state.tasks = state.tasks.filter(task => task.id !== id)
+        },
+
+    },
+    actions: {
+        addTask({ commit }, newtask) {
+            commit('ADD_TASK', newtask)
+        },
+        deleteTask({ commit }, id) {
+            commit('DELETE_TASK', id)
+        },
+    },
+    modules: {},
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,85 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-text-field class="mx-2 mt-2"
+      filled
+      label="Add Task"
+      v-model="newTask"
+      append-icon="mdi-plus"
+      @keyup.enter="addTask"
+      @click:append="addTask"
+      clearable
+    ></v-text-field>
+    <v-list flat class="pt-0" v-if="tasks.length">
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item @click="clickTask(task.id)" :class="{'blue lighten-5' : task.done} ">
+        <template v-slot:default>
+          <v-list-item-action>
+            <v-checkbox :input-value="tasks.done" color="primary"></v-checkbox>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>{{task.title}}</v-list-item-title>
+          </v-list-item-content>
+           <v-list-item-action>
+          <v-btn icon @click="deleteTask(task.id)">
+            <v-icon color="red lighten-1">mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
+        </template>
+      </v-list-item>
+      <v-divider></v-divider>
+      </div>
+    </v-list>
+    <div v-else class="notask">
+        <v-btn icon>
+          <v-icon color="red lighten-1">mdi-alert</v-icon>
+        </v-btn>
+        <div class="text-h5">  No Task</div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import {mapState} from 'vuex'
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      newTask:'',
+    }
   },
+  computed:{
+    ...mapState(['tasks'])
+  },
+  methods:{
+    clickTask(id) {
+      let task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done
+    },
+    deleteTask(id) {
+       this.$store.dispatch('deleteTask',id) 
+    },
+     addTask() {
+       let addItem = {
+         id: Date.now(),
+         title:this.newTask,
+         done:false,
+       }
+       this.$store.dispatch('addTask',addItem) 
+       this.newTask=''
+  
+    },
+  }
 };
 </script>
+
+<style scoped>
+.notask {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+}
+</style>
